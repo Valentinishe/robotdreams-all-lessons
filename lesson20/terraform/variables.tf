ad19ce30070ed81b8813f87718b30d3ad69c5864
Valentyn <valentynishe.gmail.com>
2024-01-29 11:35:42 +0200

    added lesson#20 part 1 - setup

---
+++ lesson20/terraform/variables.tf
@@ -0,0 +1,50 @@
+variable "env" {
+  description = "Environment"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Public subnet CIDR"
+  type        = string
+}
+
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "Private subnet CIDR"
+  type        = string
+}
+
+variable "route_cidr" {
+  description = "Route table CIDR"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "Key pair"
+  type        = string
+}
