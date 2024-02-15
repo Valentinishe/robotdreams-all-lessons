18dc5b71e4d57b0fb4ff75279a5ee03cc8e9ef50
Valentyn <valentynishe.gmail.com>
2024-02-15 18:43:51 +0200

    added lesson#24 with basic setup infrastracture

---
+++ lesson24/Readme.md
@@ -0,0 +1,11 @@
+# robotdreams-24
+Lesson #24
+
+
+### Setup
+- clone the project by run ```git clone git@github.com:Valentinishe/robotdreams-24.git```;
+- go to root folder in the project;
+
+### Deploy Infrastructure
+- setup infrastructure by terraform, please see /terraform/Readme.md
+- setup EKL by ansible, please see /ansible/Readme.md
---
+++ lesson24/terraform/main.tf
@@ -0,0 +1,103 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "VPC" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "VPC_JENKINS"
+    ENV  = var.env
+  }
+}
+
+resource "aws_subnet" "PUBLIC_VPC" {
+  vpc_id                  = aws_vpc.VPC.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PUBLIC_SUBNET_JENKINS"
+    ENV  = var.env
+  }
+}
+
+resource "aws_subnet" "PRIVATE_VPC" {
+  vpc_id                  = aws_vpc.VPC.id
+  cidr_block              = var.private_subnet_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PRIVATE_SUBNET_JENKINS"
+    ENV  = var.env
+  }
+}
+
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.VPC.id
+  tags = {
+    Name = "IGW_JENKINS"
+    ENV  = var.env
+  }
+}
+
+resource "aws_route_table" "PUBLIC_RT" {
+  vpc_id = aws_vpc.VPC.id
+  route {
+    cidr_block = var.route_cidr
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+  tags = {
+    Name = "PUBLIC_RT_JENKINS"
+    ENV  = var.env
+  }
+}
+
+resource "aws_route_table_association" "PUBLIC_SUBNET_ASSOCIATION_JENKINS" {
+  subnet_id      = aws_subnet.PUBLIC_VPC.id
+  route_table_id = aws_route_table.PUBLIC_RT.id
+}
+
+resource "aws_security_group" "VPC_SG" {
+  name        = "VPC_SG_JENKINS"
+  description = "VPC_SG_JENKINS"
+  vpc_id      = aws_vpc.VPC.id
+
+  ingress = [
+    for port in [22, 80, 443, 8080, 9200, 5601, 5044, 3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "JENKINS_INSTANCE" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  subnet_id              = aws_subnet.PUBLIC_VPC.id
+  vpc_security_group_ids = [aws_security_group.VPC_SG.id]
+  tags = {
+    Name = "INSTANCE_JENKINS"
+    ENV  = var.env
+    Role = "Jenkins"
+  }
+}
+
+resource "aws_eip" "VPC_JENKINS_EIP" {
+  instance = aws_instance.JENKINS_INSTANCE.id
+}
